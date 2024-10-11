9f8d391ac72229db37e6007e80a2c4b7f0a3141c
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-10-11 13:32:53 +0200

    Home page is now responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Contact } from "./pages/contact.jsx"
 import { Footer } from './layouts/footer';
@@ -20,16 +20,16 @@ function Home() {
     getData();
   }, []);
 
-  return ( <main>
+  return ( <main className="flex flex-col w-screen md:grid md:grid-cols-2 lg:grid-cols-3">
   {posts.map((post) => (
-    <div id={post.id} className="flex flex-col gap-1 mb-5">
+    <div id={post.id} className="gap-1 mb-5 flex flex-1 m-auto flex-col text-center">
+      <img src={post.image.url} className="size-60" />
       <h2>{post.title}</h2>
-      <p>Rating: {post.rating}</p>
-      <img src={post.image.url} />
       <p>$ {post.price}</p>
-      <p>{post.description}</p>
+      <button className="bg-teal-100 p-1 rounded-lg w-1/2 m-auto">Add to Cart</button>
     </div>
   ))}
+  
 </main>
 )}
 
